bb37ddff9ebc62d3248b232c073b4b4b6c916a7c
huypv <huy.leemarvick@hcmut.edu.vn>
2024-05-27 19:55:19 +0700

    Feat: predict (note that this module is untesting cause my package not include keras)

--- server/server/settings.py
+++ server/server/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
+    'rest_framework',
+    'ml'
 ]
 
 MIDDLEWARE = [
@@ -73,13 +76,16 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'freedb_homiracle',
+#         'USER': 'freedb_Homiracle_admin',
+#         'PASSWORD': 'rc%kc*n8N28MYgk',
+#         'HOST': 'sql.freedb.tech',
+#         'PORT': '3306',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
